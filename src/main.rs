475dc4d6d18325a31a30637d10f45de2a582e6f2
Johan Burell <johan.burell@valtech.se>
2016-10-03 14:41:12 +0200

    Added logging locally, will replace with remote solution later. WIP.

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,44 @@
 use iron::prelude::*;
 use iron::status;*/
 
-extern crate libusb;
+//extern crate phant;
+extern crate chrono;
 
-use libusb::Context;
+use std::fs::File;
+use std::io::{Read, Write};
+//use std::time::SystemTime;
+use std::env;
+
 
 fn main() {
-	let ctx = Context::new().unwrap();
-	let mut dymo = ctx.devices().unwrap().iter().filter(|dev| {
-		let desc = dev.device_descriptor().unwrap();
-		//println!("Filter {:x} == 0x922, {:x} == 0x8003", desc.vendor_id(), desc.product_id());
-		0x922 == desc.vendor_id() && 0x8003 == desc.product_id()
- 	}).collect::<Vec<libusb::Device>>().pop().unwrap();
+ /*   let mut phant = phant::Phant::new("http://data.sparkfun.com", 
+                                      "pub"
+                                      "priv", None);*/
 
-	println!("Vendor: {}", dymo.device_descriptor().unwrap().vendor_id());
+    if env::args().count() != 3 {
+        println!("Usage: {} <device> <logfile>", env::args().nth(0).unwrap());
+        std::process::exit(1);
+    }
 
-	let mut handle = match dymo.open() {
-		Ok(handle) => Some(handle),
-		Err(e) => {println!("Error: {}", e); None},
-	};
+    let device_path = env::args().nth(1).unwrap();
+    let logfile_path = env::args().nth(2).unwrap();
 
-	match handle {
-		Some(mut handle) => {handle.reset(); println!("Active conf: {}", handle.active_configuration().unwrap()); ()},
-		None => println!("Could not reset the handle"),
- 	}
+    let mut tty_usb = File::open(device_path.clone()).ok().expect(format!("Could not open device {}", device_path).as_str());
+    let mut log_file = File::create(logfile_path.clone()).ok().expect(format!("Could not open file {} to log to", logfile_path).as_str());
+
+    let mut data: [u8; 512] = [0u8; 512];
+    loop {
+        let num_bytes = tty_usb.read(&mut data).unwrap();
+        let line = std::str::from_utf8(&data[0..num_bytes]).ok().expect("Could not convert data from tty to UTF-8 string");
+        let now = chrono::UTC::now();
+        let data_str = format!("{}: {}", now.format("%b %-d, %-I:%M:%S%.3f").to_string(), line);
+        println!("{}", data_str);
+        log_file.write(data_str.into_bytes().as_slice());
+
+//        phant.add("weight", line);
+//        println!("Result of push: {}", phant.push().ok().expect("Pushing to server did not succeed"));
+    }
+
     /*Iron::new(|_: &mut Request| {
         Ok(Response::with((status::Ok, "Hello World2!")))
     }).http("localhost:3000").unwrap();*/
